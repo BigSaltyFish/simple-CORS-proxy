bff3f9ee4652d294d393607b4548212d2fab8cef
BigSaltyFish <2983483048@qq.com>
2019-09-04 12:56:36 +0800

    the request body will be sent by piece, which can greatly accelerate the processing.

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # A Next To Simplest CORS Proxy
-just install the dependencies add run it.
-remember to configure the header fields and server origin.
-support binary request body now.
+
+Just install the dependencies add run it.(Actually, there isn't any external dependency at all.)
+Remember to configure the header fields and server origin.
+Binary request body is supported, but certainly, you won't see them properly in the shell :) they may appear to be a block of random characters. 
+
 ```shell
 npm start
 ```
+
 output messages to the shell
+
 ```shell
 npm run rec
 ```
+
 output messages to an HTML file
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let queryString = require('querystring');
 let fs = require('fs');
 
 // the header fields in the request
-// set 'withCredentials' to true if cookies needed
+// set 'withCredentials' to true if cookies are needed
 const allowHeaders = [
     'X-CSRFtoken',
     'content-type',
@@ -18,66 +18,73 @@ const allowMethods = [
 
 const serverOrigin = 'http://127.0.0.1:8000';
 
+const PROXY_PORT = 2333;
 
 http.createServer(function(request, response) {
-    let requestHeaders = request.headers;
-    let requestBody = [];
-
     if(request.method === 'OPTIONS') {
-        response.writeHead(200, {
-            'Access-Control-Allow-Origin': requestHeaders.origin,
-            'Access-Control-Allow-Methods': allowMethods.join(', '),
-            'Access-Control-Expose-Headers': 'x-csrftoken, Set-Cookie',
-            'Access-Control-Allow-Headers': allowHeaders.join(', '),
-            'Access-Control-Allow-Credentials': 'true'
-        });
-        response.end();
+        optionsHandler(request, response);
     }
     else {
-        let requestBodyType = requestHeaders['content-type'].toString().split(';')[0];
-        request.on('data', function (data) {
-            requestBody.push(data)
-        });
-
-        request.on('end', function () {
-            // console.log(`the request url: ${req.url}`);
-            let params = Buffer.concat(requestBody);
-
-            // console.log('the request headers: ');
-            // console.log(headers);
-            let forwarding = http.request(serverOrigin + request.url, {headers: requestHeaders, method: request.method}, function (res) {
-                let proxyResponseHeaders = res.headers;
-                // console.log('the response header');
-                // console.log(tmpHeaders);
-                proxyResponseHeaders['Access-Control-Expose-Headers'] = 'x-csrftoken, Set-Cookie';
-                proxyResponseHeaders['Access-Control-Allow-Origin'] = requestHeaders.origin;
-                proxyResponseHeaders['Access-Control-Allow-Methods'] = allowMethods.join(', ');
-                proxyResponseHeaders['Access-Control-Allow-Headers'] = allowHeaders.join(', ');
-                proxyResponseHeaders['Access-Control-Allow-Credentials'] = 'true';
-                response.writeHead(200, proxyResponseHeaders);
-
-                let responseBody = '';
-                res.on('data', function (chunk) {
-                    responseBody += chunk
-                });
-                res.on('end', function () {
-                    if(responseBody.length < 2000) {
-                        console.log(`the response body: ${responseBody}`);
-                    }
-                    console.log('\n');
-                    response.write(responseBody);
-                    responseBody = '';
-                    response.end();
-                })
-            });
-
-            console.log(`the request url: ${request.url}`);
-            if(requestBodyType === 'application/json') {
-                console.log(params.toString());
-            }
-            else console.log(params.toString());
-            forwarding.write(params);
-            forwarding.end();
-        });
+        forwardingHandler(request, response);
     }
-}).listen(2333);
+}).listen(PROXY_PORT);
+
+function optionsHandler(request, response) {
+    response.writeHead(200, {
+        'Access-Control-Allow-Origin': request.headers.origin,
+        'Access-Control-Allow-Methods': allowMethods.join(', '),
+        'Access-Control-Expose-Headers': 'x-csrftoken, Set-Cookie',
+        'Access-Control-Allow-Headers': allowHeaders.join(', '),
+        'Access-Control-Allow-Credentials': 'true'
+    });
+    response.end();
+}
+
+function forwardingHandler(request, response) {
+    // the buffer to store the request body. used only to log it for the data chunk is sent to the server immediately when they arrived.
+    let requestBody = [];
+    // the forwarding body type is the same as the original request body type
+    let requestBodyType = null;
+    if (request.headers['content-type']) requestBodyType = request.headers['content-type'].toString().split(';')[0];
+
+    // the forwarding method
+    let forwarding = http.request(serverOrigin + request.url, {headers: request.headers, method: request.method}, function (res) {
+        let proxyResponseHeaders = res.headers;
+        proxyResponseHeaders['Access-Control-Expose-Headers'] = 'x-csrftoken, Set-Cookie';
+        proxyResponseHeaders['Access-Control-Allow-Origin'] = request.headers.origin;
+        proxyResponseHeaders['Access-Control-Allow-Methods'] = allowMethods.join(', ');
+        proxyResponseHeaders['Access-Control-Allow-Headers'] = allowHeaders.join(', ');
+        proxyResponseHeaders['Access-Control-Allow-Credentials'] = 'true';
+        response.writeHead(200, proxyResponseHeaders);
+
+        let responseBody = '';
+        res.on('data', function (chunk) {
+            responseBody += chunk
+        });
+        res.on('end', function () {
+            if(responseBody.length < 2000) {
+                console.log(`the response body: ${responseBody}`);
+            }
+            console.log('\n');
+            response.write(responseBody);
+            responseBody = '';
+            response.end();
+        })
+    });
+
+    request.on('data', function (data) {
+        requestBody.push(data);
+        forwarding.write(data);
+    });
+
+    request.on('end', function () {
+        let params = Buffer.concat(requestBody);
+
+        console.log(`the request url: ${request.url}`);
+        if(requestBodyType === 'application/json') {
+            console.log(params.toString());
+        }
+        else console.log(params.toString());
+        forwarding.end();
+    });
+}
